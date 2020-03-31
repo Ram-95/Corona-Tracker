684c6be1ceb81acc32c7c573d244e56857ea5503
Ram-95 <ram.kottapally@gmail.com>
2020-03-31 20:52:18 +0530

    Initial Commit

---
+++ COVID19_Corona Virus_Data.py
@@ -0,0 +1,104 @@
+#WHO Data
+import csv
+import requests
+import bs4 as bs
+from datetime import date
+from prettytable import PrettyTable
+import os
+import Slack_Push_Notification as Slack
+
+filename = 'COVID-19_Global_Data.csv'
+
+today = date.today().strftime("%d-%b-%y")
+
+
+table = PrettyTable(['S.No', 'Country', 'Total Cases', 'New Cases', 'Deaths', 'New Deaths', 'Total Recovered', 'Active Cases', 'Serious/Critical'])
+
+url= "https://www.worldometers.info/coronavirus/"
+
+response = requests.get(url)
+html = response.text
+soup = bs.BeautifulSoup(html, 'lxml')
+
+t_items = soup.find('table', id="main_table_countries_today").findAll('tr')
+sno = 0
+india = []
+
+for i in t_items[1:len(t_items)-1]:
+    sno += 1
+    country = i.select_one("td:nth-of-type(1)").text.strip()
+    total_cases =  i.select_one("td:nth-of-type(2)").text.strip()
+    new_cases = 0 if i.select_one("td:nth-of-type(3)").text.strip() == '' else i.select_one("td:nth-of-type(3)").text.strip()
+    deaths = 0 if i.select_one("td:nth-of-type(4)").text.strip() == '' else i.select_one("td:nth-of-type(4)").text.strip()
+    new_deaths = 0 if i.select_one("td:nth-of-type(5)").text.strip() == '' else i.select_one("td:nth-of-type(5)").text.strip()
+    total_recovered = 0 if i.select_one("td:nth-of-type(6)").text.strip() == '' else i.select_one("td:nth-of-type(6)").text.strip()
+    active_cases = 0 if i.select_one("td:nth-of-type(7)").text.strip() == '' else i.select_one("td:nth-of-type(7)").text.strip()
+    serious = 0 if i.select_one("td:nth-of-type(8)").text.strip() == '' else i.select_one("td:nth-of-type(8)").text.strip()
+
+    table.add_row([sno, country, total_cases, new_cases, deaths, new_deaths, total_recovered, active_cases, serious])
+    
+    if country == 'India':
+        india.append(country)
+        india.append(total_cases)
+        india.append(new_cases)
+        india.append(deaths)
+        
+
+#Information about India
+country = t_items[-1].select_one("td:nth-of-type(1)").text.strip()
+total_cases =  t_items[-1].select_one("td:nth-of-type(2)").text.strip()
+deaths = t_items[-1].select_one("td:nth-of-type(4)").text.strip()
+new_cases = t_items[-1].select_one("td:nth-of-type(3)").text.strip()
+
+print(table)
+print(f'\n{country} {total_cases}\tDeaths: {deaths}')
+
+
+#Creating the CSV File if not already present
+os.chdir('C:\\Users\\shyam\\AppData\\Local\\Programs\\Python\\Python37-32\\Corona Tracker')
+files = os.listdir()
+
+if filename not in files:
+    with open(filename, 'w') as f:
+        writer = csv.writer(f, delimiter=',', lineterminator='\n')
+        writer.writerow(['DATE', 'TOTAL_CASES', 'DEATHS'])
+        print(f'\nCSV File Created Successfully.\n')
+
+
+
+#Reading the CSV File
+with open(filename, 'r') as f:
+    reader = csv.reader(f, delimiter=',', lineterminator='\n')
+    data = list(reader)
+    #print(f'{data}')
+    
+
+if data[-1][0] == 'DATE':
+    with open(filename, 'a') as f:
+        writer = csv.writer(f, delimiter = ',', lineterminator = '\n')
+        writer.writerow([today, int(total_cases.replace(',', '')), int(deaths.replace(',', ''))])
+        
+else:
+    if data[-1][0] == today:
+        data[-1][1] = str(max(int(data[-1][1].replace(',', '')), int(total_cases.replace(',', ''))))
+        data[-1][2] = str(max(int(data[-1][2].replace(',', '')), int(deaths.replace(',', ''))))
+        #print(f'{data}')
+        with open(filename, 'w') as f:
+            wr = csv.writer(f, delimiter= ',', lineterminator='\n')
+            for item in data:
+                wr.writerow(item)
+    else:
+        with open(filename, 'a') as f:
+            writer = csv.writer(f, delimiter = ',', lineterminator = '\n')
+            writer.writerow([today, int(total_cases.replace(',', '')), int(deaths.replace(',', ''))])
+    
+
+msg = f'{country} {total_cases}\tDeaths: {deaths}\n\n{india[0]}\tCases: {india[1]}({india[2]})\tDeaths: {india[3]}'
+Slack.slack_message(msg, __file__)
+print(f'{india[0]} --> Cases: {india[1]}({india[2]})\tDeaths: {india[3]}')
+
+
+ch = input('\n\nDo you want to know India\'s COVID-19 State wise Information ?(y/n): \n')
+if ch.lower() == 'y':
+    import COVID19_India_Information as COVID_Ind
+
---
+++ COVID19_India_Information.py
@@ -0,0 +1,112 @@
+#COVID 19 India Cases
+import requests
+import bs4 as bs
+import csv
+import os
+from datetime import date
+from prettytable import PrettyTable
+
+os.chdir('C:\\Users\\shyam\\AppData\\Local\\Programs\\Python\\Python37-32\\Corona Tracker')
+filename = 'COVID-19_India_Data.csv'
+state_filename = 'COVID-19_State_wise_Information.csv'
+
+today = date.today().strftime("%d-%b-%y")
+
+
+url = "http://www.mohfw.gov.in/"
+response = requests.get(url)
+html = response.text
+soup = bs.BeautifulSoup(html, 'lxml')
+
+x = soup.find('div', class_= 'content newtab').find('p').text.strip()
+
+
+t_items = soup.find('div', class_= 'content newtab').find('table').find('tbody').findAll('tr')
+head = ['S.No', 'State/UT', 'Total Active Cases', 'Discharged', 'Dead', 'Total Cases']
+
+
+#Creating a Table
+ind_table = PrettyTable(head)
+summ_table = PrettyTable(['Total', 'Total Active Cases', 'Discharged', 'Dead', 'Total Cases'])
+
+
+b = soup.findAll('div', class_='iblock_text')
+india_total = b[1].find('div').text.strip() + '(considering only Indian Citizens) = ' + b[1].find('span').text
+
+#Writing the state wise information to a file
+with open(state_filename, 'w') as f:
+    writer = csv.writer(f, delimiter= ',', lineterminator= '\n')
+    writer.writerow([x.upper() for x in head])
+
+
+for i in t_items[:len(t_items)-2]:
+    state_data = []
+    for k in i.findAll('td'):
+        state_data.append(k.text.strip())
+    state_data.append(int(state_data[2]) + int(state_data[3]) + int(state_data[4]))
+    ind_table.add_row(state_data)
+
+    with open(state_filename, 'a') as f:
+        writer = csv.writer(f, delimiter=',', lineterminator='\n')
+        writer.writerow(state_data)
+
+
+print('\n' + '*'*20 + ' INDIA - STATE WISE INFORMATION ' + x + ' ' + '*'*20 + '\n')
+print(f'{ind_table}')
+
+summ = []
+for j in t_items[-2].findAll('td'):
+    summ.append(j.text.strip())
+
+total_cases = int(summ[1].replace('#', '')) + int(summ[2].replace('#', '')) + int(summ[3].replace('#', ''))
+summ.append(total_cases)
+
+summ_table.add_row(summ)
+
+print(f'\nSummary:\n{summ_table}')
+print(f'\n[OFFICIAL DATA] - {india_total}\n')
+
+
+
+'''Code for writing to the Files.'''
+
+#Creating the CSV File if not already present
+files = os.listdir()
+
+deaths = int(summ[3])
+
+
+if filename not in files:
+    with open(filename, 'w') as f:
+        writer = csv.writer(f, delimiter=',', lineterminator='\n')
+        writer.writerow(['DATE', 'TOTAL_CASES', 'DEATHS'])
+        print(f'\nCSV File Created Successfully.\n')
+
+
+#Reading the CSV File
+with open(filename, 'r') as f:
+    reader = csv.reader(f, delimiter=',', lineterminator='\n')
+    data = list(reader)
+    #print(f'{data}')
+    
+
+if data[-1][0] == 'DATE':
+    with open(filename, 'a') as f:
+        writer = csv.writer(f, delimiter = ',', lineterminator = '\n')
+        writer.writerow([today, total_cases, deaths])
+        
+else:
+    if data[-1][0] == today:
+        data[-1][1] = str(max(int(data[-1][1].replace(',', '')), total_cases))
+        data[-1][2] = str(max(int(data[-1][2].replace(',', '')), deaths))
+
+        with open(filename, 'w') as f:
+            wr = csv.writer(f, delimiter= ',', lineterminator='\n')
+            for item in data:
+                wr.writerow(item)
+    else:
+        with open(filename, 'a') as f:
+            writer = csv.writer(f, delimiter = ',', lineterminator = '\n')
+            writer.writerow([today, total_cases, deaths])
+
+
