d2582012acc783d5cf64326eed92ba7daed760cb
Ram Babu <ram.kottapally@gmail.com>
2020-12-28 13:48:21 +0530

    Add files via upload

---
+++ Slack_Push_Notification.py
@@ -0,0 +1,13 @@
+#Slack Push Notification
+''' This script is used to get a Push Notification on Mobile using Slack.'''
+import slack
+import os
+def slack_message(message, filename='Python Terminal'):
+    token = os.environ.get('SLACK_TOKEN')
+    sc = slack.WebClient(token)
+    sc.chat_postMessage(channel= 'python-notifications',
+                        text= '<< ' + os.path.basename(filename) + ' >>\n\n' + message,
+                        username= 'Python-Script',
+                        icon_emoji= ':sunglasses:')
+
+    print('\n***** SLACK Push Notification Successfully Sent. *****\n')
